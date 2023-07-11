e0499c2ee7984bdedb1d62363ce3961e67878ad7
Eslam-Rizk <70345162+Eslam-Rizk@users.noreply.github.com>
2023-07-11 22:32:46 +0300

    Add files via upload

---
+++ esp8266 web server/esp8266_web_server/esp8266_web_server.ino
@@ -0,0 +1,55 @@
+#include <ESP8266WiFi.h>
+
+// Set the SSID and password for the access point
+const char* ssid = "ESP8266_ssid";
+const char* password = "esp8266_password";
+
+// Create an instance of the WiFiServer
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Start the access point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+
+  // Print the IP address of the access point
+  Serial.print("Access Point IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // Start the server
+  server.begin();
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the data from the client
+  String request = client.readStringUntil('\r');
+
+  // Print the request to the serial monitor
+  Serial.print("Request: ");
+  Serial.println(request);
+
+  // Send the HTTP response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); // this line is important
+
+  // Print the data received from UART
+  client.print("Data received: ");
+  client.println(Serial.readString());
+
+  // Close the connection
+  client.stop();
+}
